8da5da9fb6dd0ca9dfa505bc4e55e6a3dce961ec
clintpaul <clintpaul89@gmail.com>
2021-05-23 18:49:37 +0530

    learning about express js middleware

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,37 +1,24 @@
+const { response } = require('express')
 const express = require('express')
 const app = express()
-const {products} = require('./data')
 
-app.get('/', (req, res) => {
-    res.send('<h1> Home Page </h1><a href="/api/products">products</a>')
+// req => middleware => res 
+
+const logger = (req, res, next) => {
+    const method = req.method
+    const url = req.url
+    const date = new Date().getFullYear()
+    console.log(method, url, date)
+    next()
+}
+
+app.get('/', logger, (req, res) => {
+    res.send('Home')
 })
 
-
-app.get('/api/products', (req, res) => {
-    const newProducts = products.map((product) =>{
-        const {id,name,image} = product
-        return {id,name,image}
-    })
-    res.json(newProducts)
+app.get('/about', (req, res) => {
+    res.send('About')
 })
-
-app.get('/api/products/:productID', (req, res) => {
-    // console.log(req)
-    // console.log(req.params)
-    const {productID} = req.params
-    const singleProduct = products.find((product) => product.id === Number(productID))
-    if(!singleProduct) {
-        return res.status(404).send('Product does not exist')
-    }
-    res.json(singleProduct)
-})
-
-
-app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
-    console.log(req.params)
-    res.send('This is the review page')
-})
-
 
 app.listen(5000, () => {
 console.log('Server is listening')
